0163e645025788d3a606099df6462482f37d7538
Chinnasurya Prasad Vulavala <vulavala.c@northeastern.edu>
2025-05-22 18:19:44 -0400

    fixed i8n config

--- app/src/i18n.ts
+++ app/src/i18n.ts
@@ -2,10 +2,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // Import translations
-import enTranslations from '../public/locales/en/common.json'
-import teTranslations from '../public/locales/telugu/common.json'
-import taTranslations from '../public/locales/tamil/common.json'
-import esTranslations from '../public/locales/es/common.json'
+import enTranslations from './locales/en/common.json'
+import teTranslations from './locales/telugu/common.json'
+import taTranslations from './locales/tamil/common.json'
+import esTranslations from './locales/es/common.json'
 
 i18n
   .use(initReactI18next)
